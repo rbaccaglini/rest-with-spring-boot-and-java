44a81f7c3366ecde161db77ec23ea8d27bc353c3
Roger <roger.baccaglini@yahoo.com.br>
2024-07-15 10:23:18 -0300

    get list of person

--- src/main/java/br/com/roger/controllers/PersonController.java
+++ src/main/java/br/com/roger/controllers/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -23,4 +25,10 @@ public class PersonController {
         return service.getById(id);
     }
 
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> getAll(){
+        return service.getAll();
+    }
 }
--- src/main/java/br/com/roger/services/PersonService.java
+++ src/main/java/br/com/roger/services/PersonService.java
@@ -3,6 +3,8 @@ package br.com.roger.services;
 import br.com.roger.models.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -25,4 +27,24 @@ public class PersonService {
         return mockPerson;
     }
 
+    public List<Person> getAll(){
+        return mockPersonList();
+    }
+
+    private List<Person> mockPersonList() {
+
+        logger.info("Finding all persons!");
+
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person mockPerson = new Person();
+            mockPerson.setId(counter.incrementAndGet());
+            mockPerson.setFirstName("Person " + i);
+            mockPerson.setLastName("LastName " + i);
+            mockPerson.setAddress("Address " + i);
+            mockPerson.setGender("male");
+            persons.add(mockPerson);
+        }
+        return persons;
+    }
 }
